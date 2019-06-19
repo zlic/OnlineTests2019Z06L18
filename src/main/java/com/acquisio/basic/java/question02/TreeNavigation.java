c4c97b9b45daeb18adc7b9df967f04a483df7239
Zhen Li <zhen.li.ca@hotmail.com>
2019-06-19 00:25:15 -0400

    Add TreeNavigation answer

--- src/main/java/com/acquisio/basic/java/question02/TreeNavigation.java
+++ src/main/java/com/acquisio/basic/java/question02/TreeNavigation.java
@@ -1,10 +1,12 @@
 package com.acquisio.basic.java.question02;
 
+import java.util.Stack;
+
 /**
  * QUESTION 2: TreeNode
  * Using the tree structure define at
  * http://en.wikipedia.org/wiki/File:Binary_tree.svg,
- * add the code necessary to optain the following output.
+ * add the code necessary to obtain the following output.
  * ..2
  * ....7
  * ......2
@@ -30,6 +32,22 @@ public class TreeNavigation {
         Node n3 = new Node("9", new Node("4"));
         Node n4 = new Node("5", null, n3);
         Node n = new Node("2", n2, n4);
-        // TODO: Implement code here
+        int depth = 1;
+        nav(n, depth);
+    }
+
+    private void nav(Node n, int depth) {
+        if (n != null) {
+            for (int i = 0; i < depth * 2; i++) {
+                System.out.print(".");
+            }
+            System.out.println(n.name);
+            if (n.children != null && n.children.length > 0 && n.children[0] != null) {
+                nav(n.children[0], depth + 1);
+            }
+            if (n.children != null && n.children.length > 1 && n.children[1] != null) {
+                nav(n.children[1], depth + 1);
+            }
+        }
     }
 }
