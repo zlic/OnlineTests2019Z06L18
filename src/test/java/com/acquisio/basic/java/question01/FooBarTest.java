7d1613f7657a987ad54e197aceff8f0b9cd2e1eb
Zhen Li <zhen.li.ca@hotmail.com>
2019-06-18 23:21:54 -0400

    Add test cases for fooBar

--- src/test/java/com/acquisio/basic/java/question01/FooBarTest.java
+++ src/test/java/com/acquisio/basic/java/question01/FooBarTest.java
@@ -7,8 +7,28 @@ import static org.junit.Assert.*;
 public class FooBarTest {
 
     @Test
+    public void testFooBar1() {
+        assertEquals("1", new FooBar().fooBar(1));
+    }
+
+    @Test
     public void testFooBar3() {
         assertEquals("Foo", new FooBar().fooBar(3));
+    }
+
+    @Test
+    public void testFooBar5() {
+        assertEquals("Bar", new FooBar().fooBar(5));
+    }
+
+    @Test
+    public void testFooBar15() {
+        assertEquals("FooBar", new FooBar().fooBar(15));
+    }
+
+    @Test
+    public void testFooBar50() {
+        assertEquals("Bar", new FooBar().fooBar(50));
     }
 
 }
