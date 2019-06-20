5b6666f3133cfa7c7e34f61ebfe1afd4c9191854
Zhen Li <zhen.li.ca@hotmail.com>
2019-06-19 21:13:04 -0400

    Adding unit test of ArithmeticException for factorial

--- src/test/java/com/acquisio/basic/java/question03/IssueResolvingTest.java
+++ src/test/java/com/acquisio/basic/java/question03/IssueResolvingTest.java
@@ -1,13 +1,25 @@
 package com.acquisio.basic.java.question03;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class IssueResolvingTest {
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
     @Test
     public void factorial() throws Exception {
         assertEquals(5040, new IssueResolving().factorial(7));
+    }
+
+    @Test
+    public void factorialNegativeNumber() throws Exception {
+        exceptionRule.expect(ArithmeticException.class);
+        exceptionRule.expectMessage("Negative number doesn't have factorial.");
+        new IssueResolving().factorial(- 1);
     }
 
 }
