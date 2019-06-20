0523ff8135c57fdb44087bdff26d86db7173fedd
Zhen Li <zhen.li.ca@hotmail.com>
2019-06-20 01:21:08 -0400

    Adding unit test for Calculator

--- src/test/java/com/acquisio/basic/java/question07/CalculatorTest.java
+++ src/test/java/com/acquisio/basic/java/question07/CalculatorTest.java
@@ -1,6 +1,51 @@
 package com.acquisio.basic.java.question07;
 
+import com.acquisio.basic.java.question03.IssueResolving;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
 
-    // TODO: Insert your code here.
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void testAdd() throws Exception {
+        int result = Calculator.applyOperation(2, 2, '+');
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void testMinus() throws Exception {
+        int result = Calculator.applyOperation(2, 2, '-');
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testMultiply() throws Exception {
+        int result = Calculator.applyOperation(2, 3, '*');
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void testDivide() throws Exception {
+        int result = Calculator.applyOperation(6, 3, '/');
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testDivideZero() throws Exception {
+        assertNull(Calculator.applyOperation(6, 0, '/'));
+    }
+
+    @Test
+    public void testNotSupport() throws Exception {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("Operator 'a' not supported");
+        Calculator.applyOperation(2, 2, 'a');
+    }
+
 }
